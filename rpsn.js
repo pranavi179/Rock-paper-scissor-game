3f9e47cb093f981c120bc4b613bcf7428e8ecad1
pranavi179 <48429437+pranavi179@users.noreply.github.com>
2019-09-17 03:56:21 +0530

    updated

--- rpsn.js
+++ rpsn.js
@@ -67,6 +67,127 @@ function computerSelection() {
     var compChoice = choices[randomIndex];
     return compChoice;
 }
+ 
+function selection() {
+    // get the user and computer choice 
+    var chosen = this.id;
+    var comp = computerSelection();
+
+    // get the users chosen item
+    var chosenItem = getById(chosen);
+
+    // prepare the chosenCompItem so we can assign it to a dynamic id
+    var chosenCompItem;
+
+    if(comp === 'rock') {
+        chosenCompItem = getById('computerRock');
+    } 
+    else if(comp === 'paper') {
+        chosenCompItem = getById('computerPaper');
+    }
+    else if(comp === 'scissors') {
+        chosenCompItem = getById('computerScissors');
+    }
+
+    // show results and disable all choices so no more can 
+    // be made while waiting for the pop up to fade out 
+    show(results);
+    reappear(results);
+    disable(userChoices);
+    disable(compChoices);
+
+
+    // make the selected item stand out from the rest
+    chosenItem.classList.add('selected');
+    chosenCompItem.classList.add('selected');
+
+    // decide who wins  
+
+
+    if(chosen === comp) {
+        results.textContent = DRAW;
+        // ugly repetive code. what can I do???
+        timeout();
+        results.innerHTML += MEH;
+    } 
+    else if(chosen === 'rock' && comp === 'scissors') {
+        results.textContent = USER_WINS;
+        score += 1;
+        userScore.textContent = score;
+        timeout();
+        results.innerHTML += SMILE;
+    }
+    else if(chosen === 'paper' && comp === 'rock') {
+        results.textContent = USER_WINS;
+        score += 1;
+        userScore.textContent = score;
+        timeout();
+        results.innerHTML += SMILE;
+    }
+    else if(chosen === 'scissors' && comp === 'paper') {
+        results.textContent = USER_WINS;
+        score += 1;
+        userScore.textContent = score;
+        timeout();
+        results.innerHTML += SMILE;
+    }
+    else {
+        results.textContent = COMP_WINS;
+        computer_score +=1;
+        compScore.textContent = computer_score;
+        timeout();
+        results.innerHTML += FROWN;
+    }
+
+
+    console.log(clicks);
+}
+
+
+// utilities
+function getById(id) {
+    return document.getElementById(id);
+}   
+
+function hide(element) {
+    element.style.display = 'none';
+}
+
+function show(element) {
+    element.style.display = 'block';
+}
+
+function disappear(element) {
+    element.className = 'disappear';
+}
+
+function reappear(element) {
+    element.className = 'reappear';
+}
+
+function disable(elements) {
+    for(var i = 0; i < elements.length; i++) {
+        elements[i].disabled = true;
+        elements[i].classList.add('unselected');
+    }
+}
+
+function enable(elements) {
+    for(var i = 0; i < elements.length; i++) {
+        elements[i].disabled = false;
+        elements[i].classList.add('default');
+        elements[i].classList.remove('selected', 'unselected');
+    }
+}
+
+function timeout() {
+    setTimeout(function(){
+        disappear(results);
+            enable(userChoices);
+            enable(compChoices);
+    }, 2000)
+}
+})();
 
 
 
